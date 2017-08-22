2cbb8e9ae96842e0d0b06389ebb79a14dda39e97
Matthias Nunes <matthiasnunes@gmail.com>
2017-08-22 15:58:40 -0300

    adds marie rose

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,9 +10,25 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
+# -----------------------------------------------------------
+# The game was launched March 26, 2016 - we take this into consideration when inserting birthdays
+
+# Least Favourite Food
+
+# Iced Coffee, Sushi (Marie Rose)
+# Fruit Punch, Round Watermelon (Honoka)
+# Cream Soda, Cabbage (Kasumi)
+# Iced Cafe Au Lait, Cabbage (Ayane)
+# Lemon Soda, Chocolate (Kokoro)
+# Blancmange, Fresh Banana (Nyotengu)
+# Cheesecake, Cream Puff (Hitomi)
+# Iced Tea, Fried Chicken (Momiji)
+# Orange Juice, Anmitsu (Helena)
+# -----------------------------------------------------------
+
 alias Doax3.{Repo, Characters.Girl}
 
-honoka = %Girl{
+_honoka = %Girl{
   name: "Honoka",
   nationality: "Japanese",
   age: 18,
@@ -24,5 +40,20 @@ honoka = %Girl{
   hip: 91,
   hobby: "Watching pro wrestling and martial arts movies, taking baths",
   favorite_color: "Cream"
+}
+|> Repo.insert!
+
+_marie_rose = %Girl{
+  name: "Marie Rose",
+  nationality: "Swedish",
+  age: 18,
+  birthday: %Ecto.Date{ year: 1997, month: 6, day: 6},
+  height: 147,
+  weight: 39,
+  bust: 74,
+  waist: 56,
+  hip: 76,
+  hobby: "Sewing, Watching horror movies",
+  favorite_color: "Deep blue"
 }
 |> Repo.insert!
